4ca66df399f5aba92e8277a442f45528d0813626
Joseph Greshik <jgreshik@ck182-22.mines.edu>
2019-01-12 23:53:31 -0700

    added maptorange, escapetime and renderescapetime fns

--- Mandelbrot.java
+++ Mandelbrot.java
@@ -18,8 +18,32 @@ class Mandelbrot{
         height=H;
         values=new double[W][H];
     }
-    void renderescapetime(int count){
-
+    // convert from from position x bounded by |a & b| to y bounded by |c & d| 
+    double maptorange(double a, double b, double c, double d, double x){
+        return (x-a)/(b-a)*(d-c)+c;
     }
-    
+    int escapetime(int i,int j,int iterations){
+        double real=maptorange(0,width,left,right,i);
+        double imag=maptorange(0,height,down,up,j);
+        Complex c=new Complex(real,imag);
+        Complex z=new Complex(0,0);
+        int count=0;
+        // here assuming square image
+        // using z^2 iterative formula
+        while(z.r*z.r+z.i*z.i<=right*right&&count<iterations){
+            z=z.multiply(z);
+            z=z.add(c);
+            count++;
+        }
+        return count;
+    }
+    void renderescapetime(int iterations){
+        int i,j;
+        for(i=0;i<width;i++){
+            for(j=0;j<height;j++){
+                values[i][j]=escapetime(i,j,iterations);
+            }
+        }
+    }
+
 }
