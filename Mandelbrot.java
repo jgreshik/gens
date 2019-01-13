e0cecf2ad9d4a789f875db5b0a3ee813cf2e6e2d
Matthew Baldin <matthewbaldin@isengard.Mines.EDU>
2019-01-12 22:47:18 -0700

    finished buddhabrot

--- Mandelbrot.java
+++ Mandelbrot.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Mandelbrot{
     //what part of the plane are we viewing?
     double left;
@@ -18,8 +19,31 @@ class Mandelbrot{
         height=H;
         values=new double[W][H];
     }
+    //get the escape time for each mapped point
     void renderescapetime(int count){
 
     }
-    
+    //get the value for each point
+    void renderbuddhabrot(int count,int points){
+        Random random=new Random();
+        for(int p=0;p<points;++p){ 
+            double x=(right-left)*random.nextDouble()+left;
+            double y=(up-down)*random.nextDouble()+right;
+            HashSet<Complex>set=new HashSet<>();
+            int c=0;
+            Complex a=new Complex(x,y);
+            Copmlex z=new Complex(0,0);
+            while(c<count&&z.abs()<2){
+                set.add(z);
+                z=z.multiply(z).add(a);
+                ++c;
+            }
+            if(c>=count)continue;
+            for(Complex n:set){
+                int i=(int)maptorange(left,right,0,i,n.r);
+                int j=(int)maptorange(down,up,0,j,n.i);
+                values[i][j]=values[i][j]+1;
+            }
+        }    
+    }
 }
